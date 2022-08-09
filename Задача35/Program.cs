aeba5637de753d641abd25df2d7a763f00adbbb2
Kostas <langas.kostya@mail.ru>
2022-08-09 13:41:13 +0500

    Нахождение и подсчет элементов массива в заданном периоде

---
+++ Задача35/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите кол-во элементов массива, значения 
+// которых лежат в отрезке [10,99].
+int[] array = new int[123];
+for ( int i = 0; i < 123; i++)
+    array[i] = new Random().Next (0, 123);
+Console.WriteLine( "[" + string.Join("," , array)+ "]");
+int n = 0;
+for (int i = 0; i < 123; i++)
+if (array[i] >= 10 && array[i] <= 99)
+n++;
+Console.WriteLine(n);
