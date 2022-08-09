ef9fd50229e23b94a1faac1794d7163ce7c8f076
Kostas <langas.kostya@mail.ru>
2022-08-09 11:58:52 +0500

    Сумма + и - элементов массива

---
+++ Задача31/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]. 
+// Найдите сумму отрицательных и положительных элементов массива 
+
+int[] array = new int[12];
+for ( int i = 0; i < 12; i++)
+    array[i] = new Random().Next (-9, 10);
+Console.WriteLine( "[" + string.Join("," , array)+ "]");
+int sum_pol = 0;
+int sum_neg = 0;    
+for (int i = 0; i < 12; i++)
+{
+    if (array[i] > 0)
+    sum_pol = sum_pol + array[i];
+    else
+    sum_neg = sum_neg + array[i];
+}
+Console.WriteLine( "Сумма положительных равна: " + sum_pol );
+Console.WriteLine( "Сумма отрицательных равна: " + sum_neg );
